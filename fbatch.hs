e8982e5bc9c6847446a0a3e00a574b8f4fdf4d3e
Isaac Shapira <fresheyeball@gmail.com>
2014-05-23 18:18:09 -0600

    code review refactor

--- fbatch.hs
+++ fbatch.hs
@@ -3,35 +3,21 @@ module Main (main) where
 import Data.List(isInfixOf)
 import Data.String.Utils(replace)
 import System.Environment(getArgs)
+import System.FilePath((</>))
 import System.Directory(renameFile, getDirectoryContents)
 
-pathsContaining :: String -> [String] -> [String]
-pathsContaining r ps = filter (isInfixOf r) ps
+getDeltas :: String -> [String] -> [(String,String)]
+getDeltas r ps = let o = filter (r `isInfixOf`) ps
+                     d = map (replace r "") o
+                    in zip o d
 
-pathsReject     :: String -> [String] -> [String]
-pathsReject r ps = map (replace r "") ps
-
-pathsRejected   :: String -> [String] -> [String]
-pathsRejected r = (pathsReject r) . (pathsContaining r)
-
-addBaseToPaths  :: String -> [String] -> [String]
-addBaseToPaths b p = map (\x -> b ++ "/" ++ x) p
-
-getDeltas       :: String -> String -> [String] -> [(String, String)]
-getDeltas b r ps = let o = addBaseToPaths b $ pathsContaining r ps
-                       d = addBaseToPaths b $ pathsRejected   r ps
-                       in zip o d
-
-renameFromPairs :: [(FilePath, FilePath)] -> IO()
-renameFromPairs [] = return ()
-renameFromPairs (x:xs) = do
-  (uncurry renameFile) x
-  renameFromPairs xs
+renameDeltaInBase :: String -> (String, String) -> IO ()
+renameDeltaInBase b (o,d) = renameFile (b </> o) (b </> d)
 
 main :: IO()
 main = do
   [directory, reject] <- getArgs
   files               <- getDirectoryContents directory
-  let deltas = getDeltas directory reject files
-  renameFromPairs deltas
+  let deltas = getDeltas reject files
+  mapM_ (renameDeltaInBase directory) deltas
   putStrLn $ (show . length $ deltas) ++ " files renamed"
