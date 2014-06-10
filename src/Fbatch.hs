38a55170bdc23dff60ab9e68f9b3b8b08e827c60
Isaac Shapira <fresheyeball@gmail.com>
2014-06-10 01:07:32 -0600

    moe

--- src/Fbatch.hs
+++ src/Fbatch.hs
@@ -8,18 +8,22 @@ import System.Console.ANSI
 import System.Directory(renameFile, renameDirectory, getDirectoryContents, doesDirectoryExist, doesFileExist)
 import qualified Control.Monad.Parallel as P
 
-type FileName = String
+type FileName  = String
+type FileDelta = (FileName, FileName)
 
 data Rename = RenameDirectory FileName FileName
             | RenameFile      FileName FileName
             | RenameNothing
             deriving (Show, Eq)
 
+data Printable = In String Color
+               | Normally String
+
 replaceItoken :: String -> String -> (FileName, Int) -> String
 replaceItoken r r' (o, i) = let t = replace "#{i}" (show i) r'
                             in replace r t o 
 
-getDeltas :: String -> String -> [FileName] -> [(FileName, FileName)]
+getDeltas :: String -> String -> [FileName] -> [FileDelta]
 getDeltas r r' ps = let o = filter (r `isInfixOf`) ps
                         d = map (replaceItoken r r') (zip o [0..])
                     in zip o d
@@ -38,9 +42,6 @@ rename :: Rename -> IO()
 rename (RenameDirectory x y) = renameDirectory x y
 rename (RenameFile      x y) = renameFile      x y
 rename  RenameNothing        = return ()
-
-data Printable = In String Color
-               | Normally String
 
 print' :: Printable -> IO String
 print' (s `In` c) = do
